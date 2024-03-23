654d37600bb9fb86820ca151146c091ba5bacc74
Neeha-Sajid <147878646+Neeha-Sajid@users.noreply.github.com>
2024-03-24 03:23:55 +0500

    Add files via upload

---
+++ Case sensitive/case.js
@@ -0,0 +1,14 @@
+// //TYPE 1: CAMEL CASE 
+// let colorName :string= "blue"
+// console.log(colorName)
+////TYPE 2: SNAKE CASE
+// let color_name: string="Red"
+// console.log(color_name)
+//// TYPE 3 : PASCAL CASE : 
+// let ColorName: string="pinkish purple"
+// console.log(ColorName)
+////USING ALL 3 CASES:
+var seasonName = "Autumn";
+var number_of_seasons = 4;
+var IsLovely = true;
+console.log(seasonName, number_of_seasons, IsLovely);
---
+++ Case sensitive/case.ts
@@ -0,0 +1,17 @@
+// //TYPE 1: CAMEL CASE 
+// let colorName :string= "blue"
+// console.log(colorName)
+
+////TYPE 2: SNAKE CASE
+// let color_name: string="Red"
+// console.log(color_name)
+
+//// TYPE 3 : PASCAL CASE : 
+// let ColorName: string="pinkish purple"
+// console.log(ColorName)
+
+////USING ALL 3 CASES:
+// let seasonName: string= "Autumn"
+// let number_of_seasons: number= 4
+// let IsLovely: boolean= true
+// console.log(seasonName, number_of_seasons, IsLovely)
